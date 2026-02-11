96c8fc3104b70ccd55623ac8157de24db474db26
cantinbruno <133025636+cantinbruno@users.noreply.github.com>
2026-02-11 20:16:31 +0000

    t

--- includes/anime.js
+++ includes/anime.js
@@ -104,3 +104,17 @@
   loop();
 })();
 
+document.querySelectorAll('.submenu').forEach(menu => {
+  const parent = menu.parentElement;
+
+  parent.addEventListener('mouseenter', () => {
+    menu.classList.remove('flip');
+
+    const rect = menu.getBoundingClientRect();
+    const overflowRight = rect.right > window.innerWidth;
+
+    if (overflowRight) {
+      menu.classList.add('flip');
+    }
+  });
+});
